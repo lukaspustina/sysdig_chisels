fb600bdd0f565498884a95b4706bd120965f2e06
Lukas Pustina <lukas.pustina@centerdevice.com>
2014-09-11 14:54:26 +0200

    Added find_time_gap chisel

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-sysdig_chisels
-==============
+# Sysdig Chisels
 
-Chisels for sysdig
+This is my repository for [sysdig](http://www.sysdig.org) [chisels](http://www.sysdig.org/wiki/chisels-overview/).
+
+## Installation
+
+Installation is simple. Copy or clone the chisels you like to either `.`, `./chisels`, `~/.chisels` or `/usr/share/sysdig/chisels`. See man page for sysdig for details.
+
+## Chisels
+
+### Find Time Gap
+
+The `find_time_gap` chisel only shows events with a time difference to the preceeding event larger than a threshould. The default threshold is 1 sec and can be optionally changed via a parameter. Sysdig filters work as usual.
+
+#### Examples
+
+* Example only considering events from process _sysdig_ and default threshold of 1 second
+
+      sysdig -c find_gap proc.name=sysdig
+
+* Example only considering events from process _sysdig_ and threshold of 100 ms
+
+      sysdig -c find_time_gap 0.1 proc.name=sysdig
+      12646 15:29:40.765903793 (0.111808714) sysdig > switch next=14593(mongod) pgft_maj=0 pgft_min=7517 vm_size=161072 vm_rss=11032 vm_swap=0
+      440231 15:29:56.837824534 (0.102809561) sysdig > switch next=0 pgft_maj=0 pgft_min=7534 vm_size=161072 vm_rss=11032 vm_swap=0
+
---
+++ find_time_gap.lua
@@ -0,0 +1,62 @@
+-- Chisel description
+description = "finds time gaps between succeeding events larger than a threshold"
+short_description = "find time gaps between events"
+category = "misc"
+
+-- Chisel argument list
+args =
+{
+    {
+        name = "threshold",
+        description = "default 1 second, i.e., 1.0",
+        argtype = "float",
+        optional = true
+    },
+}
+
+-- Constants
+ns =1000000000
+-- Default values
+threshold = 1.0 * ns -- Threshold in ns
+
+-- Argument notification callback
+function on_set_arg(name, val)
+    if name == "threshold" then
+        threshold = tonumber(val) * ns -- Threshold in ns
+        return true
+    end
+
+    return false
+end
+
+-- Initialization callback
+function on_init()
+    -- Request the fileds that we need
+    fe_time = chisel.request_field("evt.time")
+    fe_deltatime = chisel.request_field("evt.deltatime")
+    fe_name = chisel.request_field("proc.name")
+    fe_dir = chisel.request_field("evt.dir")
+    fe_type = chisel.request_field("evt.type")
+    fe_args = chisel.request_field("evt.args")
+
+    return true
+end
+
+-- Event parsing callback
+function on_event()
+    local num = evt.get_num()
+    local time = evt.field(fe_time)
+    local deltatime = evt.field(fe_deltatime)
+    local name = evt.field(fe_name)
+    local dir = evt.field(fe_dir)
+    local e_type = evt.field(fe_type)
+    local args = evt.field(fe_args)
+
+    if deltatime >= threshold then
+      -- %evt.num %evt.time (%evt.deltatime) %proc.name %evt.dir %evt.type %evt.args
+      print(num .. " " .. time .. " (" .. deltatime / ns .. ") " .. name .. " " .. dir .. " " .. e_type .. " " .. args)
+    end
+
+    return true
+end
+
